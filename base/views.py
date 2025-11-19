4ffadc3b37486c69d979e78f30855645f4b15e6e
Ashraful Islam <ashrafdcc1502@gmail.com>
2025-11-19 21:40:45 +0600

    first commit

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from base import models as base_models
+
+# Create your views here.
+
+
+def index(request):
+    services = base_models.Service.objects.all()
+    context = {
+        "services": services
+    }
+    return render(request, "base/index.html", context)
+
+
+def service_detail(request, service_id):
+    service = base_models.Service.objects.get(id=service_id)
+    context = {
+        "service": service
+    }
+    return render(request, "base/service_detail.html", context)
